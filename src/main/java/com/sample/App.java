cd6f5586ee4d7144807063f9cd698b8c50b93de3
Vaccy <Vactouser534@wp.pl>
2024-12-14 15:06:15 +0100

    app changed

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -1,6 +1,9 @@
 package com.sample;
 
 import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
 
 public class App {
 
@@ -13,28 +16,41 @@ public class App {
             return "No options provided";
         }
 
-        int n = JOptionPane.showOptionDialog(
-            null,
-            question,
-            "Question",
-            JOptionPane.DEFAULT_OPTION,
-            JOptionPane.QUESTION_MESSAGE,
-            null,    // No custom icon
-            options, // Button labels
-            options[0] // Default button
-        );
+        JLabel questionLabel = new JLabel("<html><body style='width: 400px;'>" + question + "</body></html>");
 
-        if (n >= 0 && n < options.length) {
-            return options[n];
-        } else {
-            return "User interrupts";
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0, 1)); 
+
+        JButton[] buttons = new JButton[options.length];
+        JDialog dialog = new JDialog((Frame) null, "Question", true);
+        dialog.setLayout(new BorderLayout());
+        dialog.add(questionLabel, BorderLayout.NORTH);
+        dialog.add(buttonPanel, BorderLayout.CENTER);
+        dialog.setSize(600, 400);
+
+        final String[] selectedOption = {null};
+
+        for (int i = 0; i < options.length; i++) {
+            String option = options[i];
+            buttons[i] = new JButton("<html><body style='width: 400px;'>" + option + "</body></html>");
+            buttons[i].addActionListener(e -> {
+                selectedOption[0] = option;
+                dialog.dispose();
+            });
+            buttonPanel.add(buttons[i]);
         }
+
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+
+        return selectedOption[0] != null ? selectedOption[0] : "User interrupts";
     }
 
     public static void finish(String name) {
+        JLabel resultLabel = new JLabel("<html><body style='width: 400px; padding: 20px 0;'>Your race: " + name + "</body></html>");
         JOptionPane.showMessageDialog(
             null, 
-            "The perfect game for you: " + name, 
+            resultLabel, 
             "Result", 
             JOptionPane.INFORMATION_MESSAGE
         );
